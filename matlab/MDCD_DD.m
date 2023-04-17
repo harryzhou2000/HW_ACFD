a310ce65ac924c3e026951f63b6141d224843c3a
HarryZhou <2373256746@qq.com>
2023-04-17 08:56:55 +0800

    HW5 done

--- matlab/MDCD_DD.m
+++ matlab/MDCD_DD.m
@@ -45,3 +45,55 @@ xlabel('k');
 ylabel('Im(k'')');
 legend;
 grid on;
+
+%% EPS
+ks = linspace(0,pi, 10001);
+epss = 0.01;
+alphas = linspace(0,0.2,10001);
+kss = nan * alphas;
+
+for i = 1:numel(alphas)
+    alphac = alphas(i);
+    errr  = ks - fRKM(ks,alphac, 0);
+    errgood = abs(errr) <= epss;
+    errChange = [diff(double(errgood))==-1, false];
+    kmm = min(ks(errChange));
+    kss(i) = kmm;
+end
+
+figure(3);
+clf; cla; hold on;
+plot(alphas,kss)
+
+alphac = 1/30 + 0.01;
+kmax =  pi - acos((12*alpha - 1)/(18*alpha));
+kmmax = subs(RKM,k,kmax);
+s1 = symfun(kmmax - kmax - epss,alpha);
+solalpha = double(solve(s1));
+plot([solalpha,solalpha], [0.5,1.5],'--');
+legend('x^*','\alpha_{opt}');
+
+xlabel('\alpha'); ylabel('k^*'); grid on;
+
+%% RE opt
+ks = linspace(0,pi, 129);
+alphas = [solalpha,linspace(0,0.1,6)];
+clf; cla; hold on;
+for alphac = alphas
+    kms = fRKM(ks,alphac, 0);
+    if(alphac == solalpha)
+       plot(ks,kms-ks,'x-','DisplayName', sprintf('\\alpha=\\alpha_{opt}'));
+    else
+        plot(ks,kms-ks,'.-','DisplayName', sprintf('\\alpha=%.5g',alphac));
+    end
+    
+end
+% plot(ks,ks*0,'DisplayName' ,'Exact');
+xlabel('k');
+ylabel('Re(k'')-k');
+legend;
+grid on;
+plot(ks,ks*0+epss, '-k' ,'DisplayName', sprintf('k+%g', epss));
+plot(ks,ks*0-epss, '-k' ,'DisplayName', sprintf('k+%g', epss));
+ylim([-epss,epss]*1.5);
+
